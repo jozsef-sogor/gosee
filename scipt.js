157dce449bde2c1193c97b10d830689b7f68406e
jozsef sogor <sogor.jozsef98@gmail.com>
2019-05-22 11:31:15 +0200

    h

--- scipt.js
+++ scipt.js
@@ -3,6 +3,20 @@ $(document).ready(function () {
 
     $("#myVideo").get(0).play();
 
+    $(".mute-video").click(function () {
+        if ($("video").prop('muted')) {
+            $("video").prop('muted', false);
+            $(this).addClass('unmute-video');
+
+        } else {
+            $("video").prop('muted', true);
+            $(this).removeClass('unmute-video');
+        }
+        console.log($("video").prop('muted'))
+    });
+
+
+
     $(".burger-menu").click(function () {
         $("nav").toggleClass("show");
         $("header").toggleClass("full");
