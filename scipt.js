1958cbf715f02730381e0ee59c4d81b87efcd5d7
Adi George <adi_george@outlook.com>
2019-05-27 22:11:31 +0200

    asd

--- scipt.js
+++ scipt.js
@@ -20,65 +20,89 @@ $(document).ready(function () {
 
 
 
-//box opening
-$(".box").click(function(){
-  if ($(".lid").hasClass("opened")) {
-    $('#signUp').css('display','none');
-    $(".lid").removeClass("opened");
-  } else {$(".lid").addClass("opened");}
+    //box opening
+    $(".box").click(function () {
+        if ($(".lid").hasClass("opened")) {
+            $('#signUp').css('display', 'none');
+            $(".lid").removeClass("opened");
+        } else {
+            $(".lid").addClass("opened");
+        }
 
 
-  if ($(".inside").hasClass("show")) {
-    $(".inside").removeClass("show");
-  } else {$(".inside").addClass("show");}
+        if ($(".inside").hasClass("show")) {
+            $(".inside").removeClass("show");
+        } else {
+            $(".inside").addClass("show");
+        }
 
 
-  if ($(".box").hasClass("s")) {
-    $(".box").removeClass("s");
-  } else {$(".box").addClass("s");}
+        if ($(".box").hasClass("s")) {
+            $(".box").removeClass("s");
+        } else {
+            $(".box").addClass("s");
+        }
 
-});
-
-
-
-//Interaction of the SignUp modal window
-
-$('.box').click(function() {
-  $('#signUp').css('display', 'block');
-});
-
-$('#closeModal').click(function() {
-  $('#signUp').css('display','none');
-  //$('#signUp').css('animation', 'scale-down-center .8s ease-in-out');
-  $(".lid").removeClass("opened");
-  $(".inside").removeClass("show");
-  $(".box").removeClass("s");
-});
-/*
-$(document).click(function(event) {
-  //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest("#signUp,.box").length) {
-    $('#signUp').css('display','none');
-  }*/
+    });
 
 
 
-$('input[name=consent]').click(function(){
-  if($(this).is(":checked")){
-    console.log('checked');
-    $('.submitBtn').css({'color' : 'white', 'cursor' : 'pointer'});
-    $('.submitBtn').attr('disabled' , 'false');
-  }
-  else {
-    console.log('unchecked');
-    $('.submitBtn').css({'color' : 'rgba(0,0,0,0.1)', 'cursor' : 'not-allowed'});
-    $('.submitBtn').attr('disabled' , 'true');
+    //Interaction of the SignUp modal window
 
-  }
-});
+    $('.box').click(function () {
+        $('#signUp').css('display', 'block');
+    });
+
+    $('#closeModal').click(function () {
+        $('#signUp').css('display', 'none');
+        //$('#signUp').css('animation', 'scale-down-center .8s ease-in-out');
+        $(".lid").removeClass("opened");
+        $(".inside").removeClass("show");
+        $(".box").removeClass("s");
+    });
+    /*
+    $(document).click(function(event) {
+      //if you click on anything except the modal itself or the "open modal" link, close the modal
+      if (!$(event.target).closest("#signUp,.box").length) {
+        $('#signUp').css('display','none');
+      }*/
 
 
-  
-    
+
+    $('input[name=consent]').click(function () {
+        if ($(this).is(":checked")) {
+            console.log('checked');
+            $('.submitBtn').css({
+                'color': 'white',
+                'cursor': 'pointer'
+            });
+            $('.submitBtn').attr('disabled', 'false');
+        } else {
+            console.log('unchecked');
+            $('.submitBtn').css({
+                'color': 'rgba(0,0,0,0.1)',
+                'cursor': 'not-allowed'
+            });
+            $('.submitBtn').attr('disabled', 'true');
+
+        }
+    });
+
+
+    //    slideshow animation
+
+//    setInterval(function () {
+//
+//       
+//        for (let i = 0; i < 3; i++) { //hide all images
+//            document.querySelectorAll(".slide-img")[i].classList.remove("shown");
+//        }
+//        document.querySelectorAll(".slide-img")[index].classList.add("shown");
+//
+//
+//        index === 3 ? index = 0 : index++;
+//    }, 4000);
+//
+
 
 });
